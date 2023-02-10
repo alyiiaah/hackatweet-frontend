313cef9bcbe19b4e8bf48fbdabfd3b5d93d7e578
Naoëlle <alyiiaah@live.fr>
2023-02-10 09:56:36 +0100

    first commit naoelle

--- components/Home.js
+++ components/Home.js
@@ -1,15 +1,100 @@
-import styles from '../styles/Home.module.css';
+import React, { useState } from "react";
+import styles from "../styles/Home.module.css";
+import { Button, Modal } from 'antd';
 
-function Home() {
+const Home = () => {
+  const [signInPopupIsOpen, setSignInPopupIsOpen] = useState(false);
+  const [signUpPopupIsOpen, setSignUpPopupIsOpen] = useState(false);
+  console.log("signInPopupIsOpen = " + signInPopupIsOpen);
+
+  const openSignInPopup = () => {
+    setSignInPopupIsOpen(true);
+  };
+
+  const closeSignInPopup = () => {
+    if (signInPopupIsOpen) {
+      setSignInPopupIsOpen(false);
+    }
+  };
+
+  const openSignUpPopup = () => {
+    setSignUpPopupIsOpen(true);
+  };
+
+  const closeSignUpPopup = () => {
+    if (signUpPopupIsOpen) {
+      setSignUpPopupIsOpen(false);
+    }
+  };
+
   return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <img src="tweetBackground.png" alt="Image" />
+      </div>
+      <div className={styles.right}>
+        {
+          <img
+            className={styles.twitterLogo}
+            src="twitterLogo.png"
+            alt="Image"
+          />
+        }
+        <h1 className={styles.titleh1}>
+          See what's <br />
+          happening
         </h1>
-      </main>
-    </div>
-  );
-}
+        <h2 className={styles.titleh2}>Join hackatweet today.</h2>
+        <form className={styles.formContainer}>
+          <button
+            type="submit"
+            className={styles.signUpButton}
+            onClick={openSignUpPopup}
+          >
+            Sign up
+          </button>
+          <p className={styles.text}>Already have an account?</p>
+          <button
+            type="submit"
+            className={styles.signInButton}
+            onClick={openSignInPopup}
+          >
+            Sign in
+          </button>
+        </form>
 
+        <Modal
+          isOpen={signInPopupIsOpen}
+          onRequestClose={closeSignInPopup}
+          contentLabel="Sign In Popup"
+          className={styles.popup}
+        >
+          <form>
+            <div className={styles.formGroup}>
+              <input
+                className="inputText"
+                type="text"
+                id="username"
+                name="username"
+                placeholder="username"
+              />
+              <input
+                className="inputText"
+                type="text"
+                id="password"
+                name="password"
+                placeholder="password"
+              />
+              <button type="button" className={styles.signInButton}>
+                Sign in
+              </button>
+            </div>
+            <button type="button" onClick={closeSignInPopup}>
+              Close
+            </button>
+          </form>
+        </Modal>
+
+    
 export default Home;
+
